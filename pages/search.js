74d6d9800956a318a1513929499e26a5d3ee086c
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-04-16 02:35:51 +0530

    Header complete for the search page

---
+++ pages/search.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import Header from '../components/Header';
+
+function Search() {
+	return (
+		<div>
+			<head>
+				<title>Search Result </title>
+			</head>
+			{/* Header */}
+			<Header />
+
+			{/* Search */}
+		</div>
+	);
+}
+
+export default Search;
