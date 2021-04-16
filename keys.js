732682455cee4d2e95e3fd301f2f9e6d421ab3e8
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-04-16 20:41:43 +0530

    Search Page compleyte and Google search Api is added

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API Key - https://developers.google.com/custom-search/using_rest
+export const API_KEY = 'AIzaSyCuFDbGZNk8tPgZVtZMyu7_1QWSZLte9Os';
+
+//Context Key - https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = '0e61a0bf92a986c31';
