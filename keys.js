bac495f998819a940bf2c2d90340e139170aaa24
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-04-16 20:49:02 +0530

    Done with the app and removed the api and context key for publishing it to github

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // API Key - https://developers.google.com/custom-search/using_rest
-export const API_KEY = 'AIzaSyCuFDbGZNk8tPgZVtZMyu7_1QWSZLte9Os';
+export const API_KEY = '';
 
 //Context Key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = '0e61a0bf92a986c31';
+export const CONTEXT_KEY = '';
